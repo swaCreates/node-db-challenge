e42fa39c85f320714326d7898ab264153392d11c
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-05-02 13:13:34 -0400

    fetch, create, getRes fns are working

--- project-api/project-router.js
+++ project-api/project-router.js
@@ -3,6 +3,7 @@ const db= require('./projectDB.js');
 
 const router= express.Router();
 
+// GET projects
 router.get('/', async (req, res, next) => {
     try {
         const pjs= await db.fetch();
@@ -13,6 +14,7 @@ router.get('/', async (req, res, next) => {
     };
 });
 
+// CREATE project
 router.post('/', async (req, res, next) => {
     try {
         const newProject= await db.create(req.body);
@@ -23,14 +25,39 @@ router.post('/', async (req, res, next) => {
     };
 });
 
+// GET resources
 router.get('/:id/resources', async (req, res, next) => {
     try {
-        
+        const pjResources= await db.getResources(req.params.id);
+        res.json(pjResources);
     } catch (err) {
         console.log('Error getting resources:', err);
         next(err);
     };
 });
 
+// CREATE resource
+router.post('/:id/resources', async (req, res, next) => {
+    try {
+        const payload= {
+            name: req.body.name,
+            resource_description: req.body.resource_description
+        }
+
+        const newResrc= await db.addResource(payload, req.params.id);
+
+        if(req.params.id){
+            console.log(newResrc);
+            res.status(201).json(newResrc);
+        } else{
+            res.status(404).json({
+                message: 'Invalid id or post does not exist'
+            });
+        }
+    } catch (err) {
+        console.log('Error creating resource:', err);
+        next(err);
+    };
+});
 
 module.exports= router;
--- project-api/projectDB.js
+++ project-api/projectDB.js
@@ -3,6 +3,10 @@ const db= require('../data/db_config.js');
 module.exports= {
     fetch,
     create,
+    getResources,
+    addResource,
+    getTasks,
+    addTask,
 }
 
 function fetch(){
@@ -15,17 +19,30 @@ function create(newProject){
 };
 
 function getResources(pj_id){
-
+    return db.select('name', 'resource_description')
+        .from('Project_Task')
+        .join('Resource', 'Resource.id', 'Project_Task.res_id')
+        .where('Project_Task.pj_id', pj_id);
 };
 
-function addResources(pj_id){
-
+function addResource(newResrc, pj_id){
+    return db.select('name', 'resource_description')
+        .from('Resource')
+        .insert(newResrc)
+        .join('Project_Task', 'Project_Task.res_id', 'Resource.id')
+        .join('Project', 'Project.id', 'Project_Task.id')
+        .where('Project_Task.pj_id', pj_id);
 };
 
 function getTasks(pj_id){
 
 };
 
-function addTasks(pj_id){
+function addTask(pj_id){
 
 };
+
+// ('Resource')
+//         .join('Project_Task',)
+//         .insert(newResrc)
+//         .where('')
